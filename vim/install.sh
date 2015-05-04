a0e3e0bc5398418915e83df4c3dad92c73a8e918
Doug Sweetser <dsweetser@blackducksoftware.com>
2015-05-04 11:22:09 -0400

    Updated to handle YouCompleteMe

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,6 @@
 hg clone https://vim.googlecode.com/hg/ vim
 cd vim/src
-/configure --enable-python3interp=dynamic --enable-perlinterp  --with-features=huge --enable-rubyinterp --prefix=$HOME/opt/vim
+./configure --enable-python3interp=dynamic  --enable-pythoninterp --enable-perlinterp  --with-features=huge --enable-rubyinterp --prefix=$HOME/opt/vim
 make && make install
 cd $HOME/bin
 ln -s $HOME/opt/vim/bin/vim
--- vim/update.sh
+++ vim/update.sh
@@ -3,5 +3,5 @@ hg pull
 hg update
 cd src
 make distclean
-./configure --enable-python3interp=dynamic --enable-perlinterp --with-features=huge --prefix=$HOME/opt/vim
+./configure --enable-pythoninterp --enable-python3interp=dynamic --enable-perlinterp --with-features=huge --enable-rubyinterp --prefix=$HOME/opt/vim
 make -v && make install
